53f1db5668d82a715b8f80544f7affb23ad6bb8b
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-07-06 01:39:03 +0700

    feat: add DB query for user

---
+++ src/dao/userDao.ts
@@ -0,0 +1,107 @@
+import ErrorHandler from "../utils/errorHandler";
+import bcryptjs from "bcryptjs";
+import { prisma } from "../config/dbConnection";
+import { registrationData } from "../utils/type";
+
+// create new user to DB
+const postCreateUser = async (userData: registrationData) => {
+  try {
+    const { real_name, username, password } = userData;
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = await prisma.users.create({
+      data: {
+        real_name: real_name,
+        username: username,
+        password: hashedPassword,
+      },
+      select: {
+        ID: true,
+        username: true,
+        real_name: true,
+      },
+    });
+    return newUser;
+  } catch (error: any) {
+    console.error(error);
+    throw new ErrorHandler({
+      success: false,
+      status: error.status,
+      message: error.message,
+    });
+  }
+};
+
+// get user from DB
+const getUserByUsername = async (username: string) => {
+  try {
+    const user = await prisma.users.findUnique({
+      where: { username: username },
+      select: {
+        ID: true,
+        username: true,
+        password: true,
+      },
+    });
+    return user;
+  } catch (error: any) {
+    console.error(error);
+    throw new ErrorHandler({
+      success: false,
+      status: error.status,
+      message: error.message,
+    });
+  }
+};
+
+// update token user
+const putUpdateToken = async (username: string, token: string) => {
+  try {
+    const user = await prisma.users.update({
+      where: { username: username },
+      data: {
+        token: token,
+      },
+    });
+    return user;
+  } catch (error: any) {
+    console.error(error);
+    throw new ErrorHandler({
+      success: false,
+      status: error.status,
+      message: error.message,
+    });
+  }
+};
+
+// get user from DB
+const getUserByID = async (userId: number) => {
+  try {
+    const user = await prisma.users.findUnique({
+      where: { ID: userId },
+      select: {
+        ID: true,
+        username: true,
+        real_name: true,
+        created_date: true,
+        updated_date: true,
+      },
+    });
+    if (!user) {
+      throw new ErrorHandler({
+        success: false,
+        message: "user not found",
+        status: 404,
+      });
+    }
+    return user;
+  } catch (error: any) {
+    console.error(error);
+    throw new ErrorHandler({
+      success: false,
+      status: error.status,
+      message: error.message,
+    });
+  }
+};
+
+export { postCreateUser, getUserByUsername, putUpdateToken, getUserByID };
